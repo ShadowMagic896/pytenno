6bc0f3fb41ebdb7d487485e88d01d487f383d9b2
Ryan Peckham <86629697+ShadowMagic896@users.noreply.github.com>
2022-07-08 01:30:02 -0400

    Add basic documentation

--- pytenno/client.py
+++ pytenno/client.py
@@ -1,5 +1,4 @@
 import aiohttp
-from pytenno.errors import Missing
 from pytenno.models.auctions import RivenAuction
 from pytenno.models.enums import Element, Platform, Polarity, RivenStat
 from pytenno.models.locations import Location
@@ -84,10 +83,35 @@ class PyTenno:
 
 
 class AuctionEntries(AuctionEntriesBackend):
+    """
+    A class for getting information about specific auction entries from IDs.
+    """
     async def get_by_id(self, auction_id: str) -> AuctionEntryExpanded:
+        """Gets a specific auction entry by ID.
+
+        Parameters
+        ----------
+        `auction_id`: :class:`str`
+            The ID of the auction entry to get.
+
+        Returns
+        -------
+        :class:`AuctionEntryExpanded`
+        """
         return await self._get_by_id(auction_id)
 
     async def get_bids_by_id(self, auction_id: str) -> AuctionEntryExpanded:
+        """Gets all bids for a specific auction entry by ID.
+
+        Parameters
+        ----------
+        `auction_id`: :class:`str`
+            The ID of the auction entry to get bids for.
+
+        Returns
+        -------
+        :class:`AuctionEntryExpanded`
+        """
         return await self._get_bids_by_id(auction_id)
 
 
@@ -115,8 +139,8 @@ class Auctions(AuctionsBackend):
     async def find_riven_auctions(
         self,
         *,
-        platform: Platform = Platform.pc,
         weapon_url_name: str,
+        platform: Platform = Platform.pc,
         mastery_rank_min: int = None,
         mastery_rank_max: int = None,
         re_rolls_min: int = None,
@@ -133,6 +157,33 @@ class Auctions(AuctionsBackend):
         operation: Optional[Literal["anyOf", "allOf"]] = None,
         buyout_policy: Optional[Literal["with", "direct"]] = None,
     ) -> list[AuctionEntryExpanded]:
+        """Finds all riven auctions that match the given criteria.
+        
+        Parameters
+        ----------
+        `weapon_url_name`: :class:`str`
+            The URL name of the weapon to search for.
+        `platform`: :class:`Platform`
+            The platform to search for riven auctions on. Default: :class:`Platform.pc`.
+        `mastery_rank_min`: :class:`int`
+            The minimum mastery rank of the riven. Default: :class:`None`.
+        `mastery_rank_max`: :class:`int`
+            The maximum mastery rank of the riven. Default: :class:`None`.
+        `re_rolls_min`: :class:`int`
+            The minimum number of re-rolls of the riven. Default: :class:`None`.
+        `re_rolls_max`: :class:`int`
+            The maximum number of re-rolls of the riven. Default: :class:`None`.
+        `positive_stats`: :class:`list` of :class:`RivenStat`
+            Restricts the riven to have the given positive stats. Maximum amount is 3. Default: :class:`None`.
+        `negative_stats`: :class:`list` of :class:`RivenStat`
+            Restricts the riven to have the given negative stats. Maximum amount is 3. Default: :class:`None`.
+        `polarity`: :class:`Polarity`
+            The polarity of the riven. Default: :class:`Polarity.any`.
+
+        Returns
+        -------
+        :class:`list`[:class:`AuctionEntryExpanded` ]   
+        """
         return await self._find_riven_auctions(
             platform=platform,
             weapon_url_name=weapon_url_name,
@@ -166,6 +217,33 @@ class Auctions(AuctionsBackend):
         ] = "price_desc",
         buyout_policy: Optional[Literal["with", "direct"]] = None,
     ) -> list[AuctionEntryExpanded]:
+        """Finds all lich auctions that match the given criteria.
+
+        Parameters
+        ----------
+        `weapon_url_name`: :class:`str`
+            The URL name of the weapon to search for.
+        `platform`: :class:`Platform`
+            The platform to search for lich auctions on. Default: :class:`Platform.pc`.
+        `element`: :class:`Element`
+            The element of the lich. Default: :class:`None`.
+        `ephemera`: :class:`bool`
+            Whether the lich is ephemeral. Default: :class:`None`.
+        `damage_min`: :class:`int`
+            The minimum damage of the lich. Default: :class:`None`.
+        `damage_max`: :class:`int`
+            The maximum damage of the lich. Default: :class:`None`.
+        `quirk_url_name`: :class:`str`
+            The URL name of the quirk of the lich. Default: :class:`None`.
+        `sort_by`: :class:`Literal`
+            The sort order of the results. Default: `"price_desc"`.
+        `buyout_policy`: :class:`Literal`
+            The buyout policy of the results. Default: :class:`None`.
+        
+        Returns
+        -------
+        :class:`list`[:class:`AuctionEntryExpanded`]
+        """
         return await self._find_lich_auctions(
             platform=platform,
             weapon_url_name=weapon_url_name,
@@ -185,6 +263,19 @@ class Auth(AuthBackend):
         email: str,
         password: str,
     ) -> CurrentUser:
+        """Logs in the user with the given credentials.
+        
+        Parameters
+        ----------
+        `email`: :class:`str`
+            The email of the user.
+        `password`: :class:`str`
+            The password of the user.
+        
+        Returns
+        -------
+        :class:`CurrentUser`
+        """
         return await self._login(email, password)
 
     async def register(
@@ -195,9 +286,39 @@ class Auth(AuthBackend):
         device_id: Optional[str] = None,
         recaptcha: Optional[str] = None,
     ) -> CurrentUser:
+        """Registers a new user with the given credentials.
+        
+        Parameters
+        ----------
+        `email`: :class:`str`
+            The email of the user.
+        `password`: :class:`str`
+            The password of the user.
+        `region`: :class:`VALID_LANGUAGES`
+            The region of the user. Default: `"en"`.
+        `device_id`: :class:`str`
+            The device ID of the user, used to identify devices between sessions. Default: :class:`None`.
+        `recaptcha`: :class:`str`
+            The Google recaptcha response of the user. Default: :class:`None`.
+        
+        Returns
+        -------
+        :class:`CurrentUser`
+        """
         return await self._register(email, password, region, device_id, recaptcha)
 
     async def recover(self, email: str) -> None:
+        """"Sends the user a recovery email.
+        
+        Parameters
+        ----------
+        email: :class:`str`
+            The email of the user.
+        
+        Returns
+        -------
+        :class:`None`
+        """
         return await self._recover(email)
 
 
@@ -207,37 +328,36 @@ class Items(ItemsBackend):
     """
 
     async def get_items(self, language: VALID_LANGUAGES = "en") -> list[ItemShort]:
-        """
-        Returns all available items
+        """Gets all items.
+        
+        Parameters
+        ----------
+        `language`: :class:`VALID_LANGUAGES`
+            The language of the items. Default: `"en"`.
+        
+        Returns
+        -------
+        :class:`list`[:class:`ItemShort`]
         """
         return await self._get_items(language)
-
-    @overload
-    async def get_droptable(
-        self,
-        item_name: str,
-        include_items: Literal[True],
-        language: VALID_LANGUAGES = "en",
-    ) -> tuple[list[DropTable], list[ItemFull]]:
-        ...
-
-    @overload
-    async def get_droptable(
-        self,
-        item_name: str,
-        include_items: Literal[False],
-        language: VALID_LANGUAGES = "en",
-    ) -> list[DropTable]:
-        ...
 
     async def get_item(
         self,
         item_name: str,
-        platform: Optional[Literal["pc", "xbox", "ps4", "switch"]] = "pc",
+        platform: Optional[Platform] = Platform.pc,
     ) -> list[ItemFull]:
-        """
-        Returns available information on an item.
-        Items that are part of a set will have the entire set returned, as well as the blueprint and set object, if available.
+        """Gets the item with the given name, as well as related items (such as items of the same set).
+        
+        Parameters
+        ----------
+        `item_name`: :class:`str`
+            The name of the item.
+        `platform`: :class:`Platform`
+            The platform of the item. Default: :class:`Platform.pc`.
+
+        Returns
+        -------
+        :class:`list`[:class:`ItemFull`]
         """
         return await self._get_item(item_name, platform)
 
@@ -246,7 +366,7 @@ class Items(ItemsBackend):
         self,
         item_name: str,
         include_items: Literal[False],
-        platform: Optional[Literal["pc", "xbox", "ps4", "switch"]] = "pc",
+        platform: Optional[Platform] = Platform.pc,
     ) -> list[OrderRow]:
         ...
 
@@ -255,7 +375,7 @@ class Items(ItemsBackend):
         self,
         item_name: str,
         include_items: Literal[True],
-        platform: Optional[Literal["pc", "xbox", "ps4", "switch"]] = "pc",
+        platform: Optional[Platform] = Platform.pc,
     ) -> tuple[list[OrderRow], list[ItemFull]]:
         ...
 
@@ -263,8 +383,23 @@ class Items(ItemsBackend):
         self,
         item_name: str,
         include_items: bool,
-        platform: Optional[Literal["pc", "xbox", "ps4", "switch"]] = "pc",
+        platform: Optional[Platform] = Platform.pc,
     ):
+        """Gets the orders of the given item.
+        
+        Parameters
+        ----------
+        `item_name`: :class:`str`
+            The name of the item.
+        `include_items`: :class:`bool`
+            Whether to include information about the item requested.
+        `platform`: :class:`Platform`
+            The platform of the item. Default: :class:`Platform.pc`.
+        
+        Returns
+        -------
+        :class:`list`[:class:`OrderRow`] | :class:`tuple`(:class:`list`[:class:`OrderRow`], :class:`list`[:class:`ItemFull`])
+        """
         return await self._get_orders(item_name, include_items, platform)
 
     @overload
@@ -290,7 +425,22 @@ class Items(ItemsBackend):
         item_name: str,
         include_items: bool,
         language: VALID_LANGUAGES = "en",
-    ):
+    ):  
+        """Gets the droptable of the given item.
+        
+        Parameters
+        ----------
+        `item_name`: :class:`str`
+            The name of the item.
+        `include_items`: :class:`bool`
+            Whether to include information about the item requested.
+        `language`: :class:`VALID_LANGUAGES`
+            The language of the droptable. Default: `"en"`.
+        
+        Returns
+        -------
+        :class:`DropTable` | :class:`tuple`(:class:`DropTable`, :class:`list`[:class:`ItemFull`])
+        """
         raise Exception(
             "The API on warframe.market for this feature is currently nonfunctional"
         )
@@ -302,46 +452,121 @@ class Liches(LichesBackend):
     """
 
     async def get_weapons(self, language: VALID_LANGUAGES = "en") -> list[LichWeapon]:
+        """Gets all weapons.
+        
+        Parameters
+        ----------
+        `language`: :class:`VALID_LANGUAGES`
+            The language of the weapons. Default: `"en"`.
+        
+        Returns
+        -------
+        :class:`list`[:class:`LichWeapon`]
+        """
         return await self._get_weapons(language)
 
     async def get_ephemeras(
         self, language: VALID_LANGUAGES = "en"
     ) -> list[LichEphemera]:
+        """Gets all lich ephemeras.
+        
+        Parameters
+        ----------
+        `language`: :class:`VALID_LANGUAGES`
+            The language of the ephemeras. Default: `"en"`.
+        
+        Returns
+        -------
+        :class:`list`[:class:`LichEphemera`]
+        """
         return await self._get_ephemeras(language)
 
     async def get_quirks(self, language: VALID_LANGUAGES = "en") -> list[LichQuirk]:
+        """Gets all lich quirks.
+        
+        Parameters
+        ----------
+        `language`: :class:`VALID_LANGUAGES`
+            The language of the quirks. Default: `"en"`.
+        
+        Returns
+        -------
+        :class:`list`[:class:`LichQuirk`]"""
         return await self._get_quirks(language)
 
 
 class Rivens(RivensBackend):
-    """
-    Provides all information about riven data models.
-    """
-
-    async def get_items(self, language: VALID_LANGUAGES = "en") -> list[RivenItem]:
+    async def get_riven_items(self, language: VALID_LANGUAGES = "en") -> list[RivenItem]:
+        """Gets a list of all riven-equippable items.
+    
+        Parameters
+        ----------
+        `language`: :class:`VALID_LANGUAGES`
+            The language of the riven items. Default: `"en"`.
+        
+        Returns
+        -------
+        :class:`list`[:class:`RivenItem`]
+        """
         return await self._get_riven_items(language)
 
     async def get_attributes(
         self, language: VALID_LANGUAGES = "en"
     ) -> list[RivenAttribute]:
+        """Gets a list of all riven attributes.
+        
+        Parameters
+        ----------
+        `language`: :class:`VALID_LANGUAGES`
+            The language of the riven attributes. Default: `"en"`.
+        
+        Returns
+        -------
+        :class:`list`[:class:`RivenAttribute`]
+        """
         return await self._get_riven_attributes(language)
 
 
 class Misc(MiscBackend):
-    """
-    Additional miscellaneous endpoints
-    """
-
     async def get_locations(self, language: VALID_LANGUAGES = "en") -> list[Location]:
-        # nonfunctional
+        """Gets a list of all locations.
+
+        Parameters
+        ----------
+        `language`: :class:`VALID_LANGUAGES`
+            The language of the locations. Default: `"en"`.
+        
+        Returns
+        -------
+        :class:`list`[:class:`Location`]
+        """
         return await self._get_locations(language)
 
     async def get_npcs(self, language: VALID_LANGUAGES = "en") -> list[NPC]:
-        # nonfunctional
+        """Gets a list of all NPCs.
+        
+        Parameters
+        ----------
+        `language`: :class:`VALID_LANGUAGES`
+            The language of the NPCs. Default: `"en"`.
+        
+        Returns
+        -------
+        :class:`list`[:class:`NPC`]
+        """
         return await self._get_npcs(language)
 
     async def get_missions(
         self, language: VALID_LANGUAGES = "en"
     ) -> list[PartialMission]:
-        # nonfunctional
+        """Gets a list of all missions.
+        
+        Parameters
+        ----------
+        `language`: :class:`VALID_LANGUAGES`
+            The language of the missions. Default: `"en"`.
+        
+        Returns
+        -------
+        :class:`list`[:class:`PartialMission`]"""
         return await self._get_missions(language)
--- pytenno/errors.py
+++ pytenno/errors.py
@@ -9,15 +9,12 @@ class BaseError(BaseException):
     def __init__(self, *args: object) -> None:
         super().__init__(f"Error [Code {self.code}]: {self.msg}")
 
-
-class Missing(BaseError):
-    code = 404
-    msg = "Undocumented"
-
+# Create an eror class for every error response and code I can recieve
+# It should properly be inherited from BaseError
 
 class BadRequest(BaseError):
     code = 400
-    msg = "Invalid request"
+    msg = "Bad Request"
 
 
 class Unauthorized(BaseError):
@@ -25,6 +22,41 @@ class Unauthorized(BaseError):
     msg = "Unauthorized"
 
 
+class Forbidden(BaseError):
+    code = 403
+    msg = "Forbidden"
+
+
+class NotFound(BaseError):
+    code = 404
+    msg = "Not Found"
+
+
+class Conflict(BaseError):
+    code = 409
+    msg = "Conflict"
+
+
 class InternalServerError(BaseError):
     code = 500
-    msg = "Internal server error"
+    msg = "Internal Server Error"
+
+
+class NotImplemented(BaseError):
+    code = 501
+    msg = "Not Implemented"
+
+
+class BadGateway(BaseError):
+    code = 502
+    msg = "Bad Gateway"
+
+
+class ServiceUnavailable(BaseError):
+    code = 503
+    msg = "Service Unavailable"
+
+
+class GatewayTimeout(BaseError):
+    code = 504
+    msg = "Gateway Timeout"
