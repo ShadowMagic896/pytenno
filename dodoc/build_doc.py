a2774729af920e621ce7915344ee7eac788a60b7
Ryan Peckham <86629697+ShadowMagic896@users.noreply.github.com>
2022-07-11 14:12:29 -0400

    Update docs

--- dodoc/build_doc.py
+++ dodoc/build_doc.py
@@ -86,6 +86,7 @@ def build_files(source: str, output: str):
         with open(f"{output}\\conf.py", "w") as target:
             target.write(template.read())
 
+    return
     print("Building html")
     os.system(f"cd ./dodoc && make")
 
